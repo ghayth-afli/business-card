9b75c512788aa9388ec056f7d746a531d40625ca
Afli Mohamed Ghayth <40703798+ghayth-afli@users.noreply.github.com>
2024-01-30 21:52:10 +0100

    add card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
